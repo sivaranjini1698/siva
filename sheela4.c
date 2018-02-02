ad9529522442c2b87d19f6c127140272edff52fc
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-02 16:48:48 +0530

    sheela4.c
    program

---
+++ sheela4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+main()
+char str[20]
+printf("enter a string");
+get(str);
+str lwr(str);
+puts("lower case string is;");
+printf("lower case string is");
+printf(str);
+}
