53f30d8c38eefc61127f9b705e5072285055b8a8
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-09 16:57:07 +0530

    stringadd.c
    program

---
+++ stringadd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>;
+#include<string.h>
+main()
+{
+char str[20],str2[20];
+int res;
+puts("enter the string1:");
+gets(str1);
+puts("enter the string2:");
+gets(str2);
+res=strcmpi9str1,str20;
+if(res==0)
+puts("string are equal");
+else
+puts("string are not equal");
+}
