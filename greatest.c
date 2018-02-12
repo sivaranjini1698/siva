08741c23826738dee50692ddb0f5d729fb3d9a77
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-12 16:08:03 +0530

    greatest.c
    program

---
+++ greatest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include>conio.h>
+{
+int num a=5,b=10,k;
+clrscr()
+printf("enter the number");
+scanf("%d,%d &a,&b,k);
+}
+if(a>b)
+printf("%d is greater then a");
+else
+{
+printf("%d is greater then b");
+}
