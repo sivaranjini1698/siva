8fe732b86f3d1f5f3940dbcaf01542f8d68011a9
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-21 16:03:05 +0530

    ascending.c
    program

---
+++ ascending.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+int num;
+printf("enter the number");
+scanf("%d",& num);
+{
+for(i=0;i<10:i++)
+printf("enter the acending numbers");
+scanf("%d",& acsending number");
+{
+else
+printf("descending number")
+}
+}
