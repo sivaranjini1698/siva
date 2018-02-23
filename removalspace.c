046aa502cd6696b1d0f24a267944c32186670638
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-23 16:10:01 +0530

    removalspace.c
    program

---
+++ removalspace.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+voidmain()
+{
+char str[80];
+int i=0,len,j;
+clrscr(0;
+printf("enter the string\n");
+gets(str);
+len=strnglen(str);
+for(i=0;i<len;i++)
+{
+if(str[i]==' ');
+{
+for(j=0;j<len;j++)
+}
