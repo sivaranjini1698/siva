77e699be3936e15f669108808caab20aa35fb097
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-26 02:44:50 -0800

    square.c
    program

---
+++ square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int n;
+clrscr()
+printf("enter the numbers");
+scanf("%d",& n);
+for(i=0;i<10;i++);
+{
+square=2^n
+}
+printf("single square value");
+}
