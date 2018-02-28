6ae8acfda0c1b32dbd547314fff0eb0f03fe3eeb
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-28 15:38:56 +0530

    string6.c
    program

---
+++ string6.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+intmain()
+{
+int a;
+printf("enter the valuae");
+scanf("%d",& a);
+for(i=0;i<10;i++);
+{
+printf("the value is even")
+else
+{
+printf("the value is odd")
+}
+}
