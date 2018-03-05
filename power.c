cba91378de98ea26790cf23e2463e6f435b77dfd
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-03-05 16:28:36 +0530

    power.c
    program

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m;
+printf("check whether it is power of 2");
+scanf('%d" &,n);
+n=(&n(n-1)
+{
+if(m==n)
+{
+printf("it is power of 2");
+}
+}
+}
