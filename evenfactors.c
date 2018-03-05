e79b99cce299c861ad445466ce6d104e90db1ab2
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-03-05 16:11:50 +0530

    evenfactors.c
    program

---
+++ evenfactors.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr()
+printf("enter the  even number");
+scanf("%d" ,& even num);
+for(i=o;i<10;i++);
+{
+sum=fact+i;
+}
+printf("even numbers");
+}
+
