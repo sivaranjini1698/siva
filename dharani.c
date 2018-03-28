93146fad55d90da2a13b990b68e780c54c79ec83
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-03-28 16:12:05 +0530

    dharani.c
    program

---
+++ dharani.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+{ 
+int num;
+printf("enter the numbers");
+scanf("%d" &,num);
+for(i=0;i<10;i++);
+printf("odd numbers");
+else
+{
+printf("even numbers");
+}
+}
